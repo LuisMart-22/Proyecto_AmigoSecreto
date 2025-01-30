91e8a40c6ebb4b0b9e0e461420ff1b87b69b899a
Jose Maartinez <jozlu22@gmail.com>
2025-01-30 10:09:29 -0500

    Actualizando el archiv app.js

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+console.log("Hola Jolu")// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
