6b344b82bbf27561cb7ffbbbeb2a92f2864e9bd6
Jose Maartinez <jozlu22@gmail.com>
2025-01-30 11:10:48 -0500

    Funcion agregatAmigo, ya realizado

--- app.js
+++ app.js
@@ -5,9 +5,27 @@ let amigos = [];
 function agregarAmigo() {
     // Obtiene el nombre del amigo.
     let nombre = document.getElementById('amigo').value;
-    // Agrega el nombre al array de amigos.
-    amigos.push(nombre);
-    // Limpia el input.
-    document.getElementById('amigo').value = '';
-    console.log(amigos);
+    //Evalúo que el nombre ingresado no sea vacío.
+    console.log(typeof(nombre));
+    if (nombre === '') {
+        alert('Debes ingresar un nombre.');
+        return;
+    } else {
+        //Evalúo que el nombre ingresado no sea un número.
+        if (!isNaN(nombre)) {
+            alert('El nombre no puede ser un número.');
+            return;     
+        } else {
+            if (amigos.includes(nombre)) {
+                alert('El nombre ya fue agregado, ingrese uno nuevo.');
+                return;
+            } else {
+                // Agrega el nombre al array de amigos.
+                amigos.push(nombre);
+                // Limpia el input.
+                document.getElementById('amigo').value = '';
+                console.log(amigos);
+            }    
+        }
+    } 
 }
